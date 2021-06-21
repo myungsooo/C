6297fd22e9b7d1611fbf741cb9e24c7be96678e1
soo <audtn0508@naver.com>
2021-06-21 23:58:21 +0900

    Casting

---
+++ 9.Casting/01.c
@@ -0,0 +1,13 @@
+/* 무시 */
+#include <stdio.h>
+
+int main() {
+    int a;
+    double b;
+
+    b = 2.4;
+    a = b;
+
+    printf("%d", a);
+    return 0;
+}
---
+++ 9.Casting/03.c
@@ -0,0 +1,12 @@
+/* 형변환 */
+#include <stdio.h>
+int main() {
+    int a;
+    double b;
+
+    b = 2.4;
+    a = (int)b;
+
+    printf("%d", a);
+    return 0;
+}
